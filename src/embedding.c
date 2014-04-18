e85b2d0f35af4d0b2529b64185f639a4c4f39163
armgong <armgong@yahoo.com>
2014-04-18 10:56:25 +0800

    write simple code to test

--- src/embedding.c
+++ src/embedding.c
@@ -3,14 +3,29 @@
 #include <stdio.h>
 #include <math.h>
 
-void initJulia(char* JuliaHome)
+void initJulia()
 {
-  jl_init(JuliaHome);
+  jl_init(NULL);
   JL_SET_STACK_BASE;
 }
 
-jl_value_t jl_eval(char* str)
+void jl_eval(char* str)
 {
  jl_value_t *ret = jl_eval_string(str);
+ double retDouble = jl_unbox_float64(ret);
+ Rprintf("test value is %f",retDouble);
+ R_FlushConsole();
 }
+
+/*jl_value_t* jl_eval(char* str)
+{
+ jl_value_t *ret = jl_eval_string(str);
+  if (jl_is_float64(ret)) {
+            double retDouble = jl_unbox_float64(ret);
+            Rprintf("test value is %e",retDouble);
+        }
+
+ return ret;
+}
+*/
 
