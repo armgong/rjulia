5b742a74a8f997389742af222b99684d35df0d75
armgong <armgong@yahoo.com>
2014-04-18 14:01:12 +0800

    mini system is done ,julia can init and eval command success

--- src/embedding.c
+++ src/embedding.c
@@ -1,31 +1,37 @@
-#include <R.h>
 #include <julia.h>
 #include <stdio.h>
 #include <math.h>
-
-void initJulia()
+#include <R.h>
+#include <Rinternals.h>
+#include <Rmath.h>
+SEXP initJulia(SEXP julia_home)
 {
-  jl_init(NULL);
-  JL_SET_STACK_BASE;
+    if (jl_is_initialized())
+	 return R_NilValue;
+
+    const char* s = CHAR(STRING_ELT(julia_home, 0));
+    if (strlen(s)==0)
+     jl_init(NULL);
+    else
+     jl_init(s);
+    JL_SET_STACK_BASE;
+    if (jl_exception_occurred()) {
+       error("Julia not initialized");
+    }
+    return R_NilValue;
 }
 
-void jl_eval(char* str)
+SEXP jl_eval(SEXP cmd)
 {
- jl_value_t *ret = jl_eval_string(str);
+ const char* s = CHAR(STRING_ELT(cmd, 0));
+ jl_value_t *ret = jl_eval_string(s);
  double retDouble = jl_unbox_float64(ret);
- Rprintf("test value is %f",retDouble);
- R_FlushConsole();
+ 
+ if (jl_is_float64(ret)) {
+	double retDouble = jl_unbox_float64(ret);
+	Rprintf("test value is %e",retDouble);
+ }
+ return R_NilValue;
 }
 
-/*jl_value_t* jl_eval(char* str)
-{
- jl_value_t *ret = jl_eval_string(str);
-  if (jl_is_float64(ret)) {
-            double retDouble = jl_unbox_float64(ret);
-            Rprintf("test value is %e",retDouble);
-        }
-
- return ret;
-}
-*/
 
