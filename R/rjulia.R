edad2d36eb8c5fe4a4a6e6b7d0b09dc111ac9c04
armgong <armgong@yahoo.com>
2014-06-09 08:47:26 +0800

    add R code guess julia home and avoid julia not exist crush R

--- R/rjulia.R
+++ R/rjulia.R
@@ -1,6 +1,49 @@
+JuliaIsExist<-function(juliahome) 
+{  
+ if (nchar(juliahome)>0)
+ {  
+   jhome<-juliahome
+ }
+ else
+ {
+   jhome<-""
+   if (nchar(Sys.getenv("JULIA_HOME"))>0)
+     jhome<-Sys.getenv("JULIA_HOME")
+   if  (nchar(Sys.getenv("Julia_Home"))>0)
+     jhome<-Sys.getenv("Julia_Home")
+   if  (nchar(Sys.getenv("JULIAHOME"))>0)
+     jhome<-Sys.getenv("JULIAHOME")
+   if  (nchar(Sys.getenv("JuliaHome"))>0)
+     jhome<-Sys.getenv("JuliaHome")
+   if  (nchar(Sys.getenv("JULIA"))>0)
+     jhome<-Sys.getenv("JULIA")
+   if  (nchar(Sys.getenv("Julia"))>0)
+     jhome<-Sys.getenv("Julia")
+ }
+  
+ if (nchar(jhome)>0)
+ { 
+   if ((jhome[length(jhome)]!="/")||(jhome[length(jhome)]!="\\"))
+     sysfile<-paste(jhome,"/../lib/julia/sys.ji",sep="")
+   else
+     sysfile<-paste(jhome,"../lib/julia/sys.ji",sep="")
+   ret<-file.exists(sysfile)
+ }  
+ else
+  {
+  	ret<-FALSE
+  }
+  return (list(ret,jhome))
+}
+ 
+
 julia_init <- function(juliahome,disablegc=TRUE)
 {
- .Call("initJulia",juliahome,disablegc,PACKAGE="rjulia")
+ findjl<-JuliaIsExist(juliahome)	
+ if (findjl[[1]])	
+  .Call("initJulia",findjl[[2]],disablegc,PACKAGE="rjulia")
+ else
+  stop("Could't Find Julia,Besure juliahome your passed is right")
 }
 
 julia_eval<-function(expression)
