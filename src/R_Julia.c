2effcb5f51ec5bc9b3eeb7e9b51ea2971b5dcc9d
armgong <armgong@yahoo.com>
2014-06-25 11:28:13 +0800

    change R string convert logical, if it is ascii then convert to julia ascii string ,otherwise convert to utf-8 string

--- src/R_Julia.c
+++ src/R_Julia.c
@@ -13,6 +13,8 @@ Copyright (C) 2014 by Yu Gong
 #define pkgdebug
 
 #define UTF8_MASK (1<<3)
+#define ASCII_MASK (1<<6)
+# define IS_ASCII(x) ((x)->sxpinfo.gp & ASCII_MASK)
 #define IS_UTF8(x) ((x)->sxpinfo.gp & UTF8_MASK)
 jl_array_t* CreateArray(jl_datatype_t* type,size_t ndim,jl_tuple_t* dims)
 {
@@ -91,15 +93,15 @@ jl_value_t* R_Julia_MD(SEXP Var,const char* VarName)
       case STRSXP:
       {
         jl_array_t* ret;
-        if (IS_UTF8(Var))
+        if (!IS_ASCII(Var))
          ret= CreateArray(jl_utf8_string_type,jl_tuple_len(dims),dims);
         else
          ret= CreateArray(jl_ascii_string_type,jl_tuple_len(dims),dims);
         JL_GC_PUSH1(&ret);
         jl_value_t** retData=jl_array_data(ret); 
         for(size_t i=0; i<jl_array_len(ret); i++)
-         if (IS_UTF8(Var))
-          retData[i] =jl_cstr_to_string(translateChar(STRING_ELT(Var, i)));  
+         if (!IS_ASCII(Var))
+          retData[i] =jl_cstr_to_string(translateChar0(STRING_ELT(Var, i)));  
          else 
           retData[i] =jl_cstr_to_string(CHAR(STRING_ELT(Var, i)));
        jl_set_global(jl_main_module, jl_symbol(VarName), (jl_value_t*)ret); 
@@ -233,7 +235,7 @@ jl_value_t* R_Julia_MD_NA(SEXP Var,const char* VarName)
     }
     case STRSXP:
     { jl_array_t* ret;
-      if (IS_UTF8(Var))
+      if (!IS_ASCII(Var))
        ret= CreateArray(jl_utf8_string_type,jl_tuple_len(dims),dims);
       else
        ret= CreateArray(jl_ascii_string_type,jl_tuple_len(dims),dims);
@@ -252,8 +254,8 @@ jl_value_t* R_Julia_MD_NA(SEXP Var,const char* VarName)
        }
        else
        {
-        if (IS_UTF8(Var))
-         retData[i] =jl_cstr_to_string(translateChar(STRING_ELT(Var, i)));  
+        if (!IS_ASCII(Var))
+         retData[i] =jl_cstr_to_string(translateChar0(STRING_ELT(Var, i)));  
         else 
          retData[i] =jl_cstr_to_string(CHAR(STRING_ELT(Var, i)));
         retData1[i] =false;
