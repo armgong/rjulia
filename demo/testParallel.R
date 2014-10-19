1ed59de1fea556132d083fc67d43f77a38e3e7f8
Yu Gong <armgong@yahoo.com>
2014-10-19 19:21:51 +0800

    fix test code ,release test workers

--- demo/testParallel.R
+++ demo/testParallel.R
@@ -7,15 +7,15 @@ if(.Platform$OS.type == "unix") julia_init("/usr/bin",F,T) else
   else 
    {julia_init("c:/julia32/bin",F,T)}  
  }
-
-julia_eval("addprocs(3)")
-for (i in 1:4)
+#warning don't add too much procs in test
+#otherwise it will crash on low end machine
+julia_eval("addprocs(1)")
+for (i in 1:2)
 {
  julia_void_eval(paste("r=remotecall(",i,", rand, 2, 2)",sep=""))
  y<-j2r(" fetch(r)")
  cat("\n")
  cat(paste("process ",i," get value:\n",sep=""))
-
  print(y)
 }
-
+julia_void_eval("rmprocs(workers())")
