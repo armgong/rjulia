19ff9284ddc53e42993523de86f29f8ca3d5c5f8
Yu Gong <armgong@yahoo.com>
2015-01-06 18:37:07 -0500

    modify Makevars and zzz.R, let RJulia can be compile and load without user set env variable

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,23 @@
-
 .onLoad <- function(libname, pkgname)
 {
-   library.dynam("rjulia",pkgname,libname,local=FALSE)
+
+ldjulia<-gsub(pattern="\"", replacement="",system('julia -E "abspath(Sys.dlpath(\\\"libjulia\\\"))"',intern=T))
+
+ #Sys.setenv("LD_LIBRARY_PATH"=paste(ldjulia,Sys.getenv("LD_LIBRARY_PATH"),sep=':'))
+  # print(Sys.getenv("LD_LIBRARY_PATH"))
+  # print(pkgname)
+  # print(libname)
+   dyn.load(ldjulia,local=F)
+   library.dynam("rjulia",pkgname,libname,local=F)
 
 }
 
 .onUnload <- function(libpath)
  {
+
+ ldjulia<-gsub(pattern="\"", replacement="",system('julia -E "abspath(Sys.dlpath(\\\"libjulia\\\"))"',intern=T))
+
+ dyn.unload(ldjulia)
+
  library.dynam.unload("rjulia", libpath)
 }
