ae2da428f7929ccfd2b98b9d7d5a7a60aad59fa0
Yu Gong <armgong@yahoo.com>
2015-08-07 09:57:51 +0800

    add more parralle demos, and add warning message about julia 0.3.x

--- test/stresstest.R
+++ test/stresstest.R
@@ -300,15 +300,15 @@ for (i in 1:1)
 
 ## warning don't add too much procs in test
 ## otherwise it will crash on low end machine
+## warning due to https://github.com/JuliaLang/julia/issues/10085, the rjulia master branch may crash when calling julia parallel functions on Julia 0.3.x. 
+
 julia_void_eval("addprocs(1)")
 
-for (i in 1:2) {
-  julia_void_eval(paste("r=remotecall(",i,", rand, 2, 2)",sep = ""))
-  y <- j2r(" fetch(r)")
-  cat("\n")
-  cat(paste("process ",i," get value:\n",sep = ""))
+julia_void_eval(paste("r=remotecall(",2,", rand, 2, 2)",sep = ""))
+y <- j2r(" fetch(r)")
+cat("\n")
+cat(paste("process ",2," get value:\n",sep = ""))
+print(y)
 
-  print(y)
-}
 julia_void_eval("rmprocs(workers())")
 
