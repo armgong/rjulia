6f28f30d35c39d1d6d51545e1fd9d7ce84d84d2e
Peter M. Haverty <phaverty@gene.com>
2016-08-01 18:06:01 -0700

    DataFrames and DataArrays are pretty central, so we should probably load them at init time

--- R/rjulia.R
+++ R/rjulia.R
@@ -12,6 +12,10 @@ julia_init <- function(juliahome="", disablegc = FALSE, parallel = TRUE)
   ## If on Windows, run a specific push to compensate for R not handling pkg.dir() correctly.
   julia_void_eval('@static if is_windows()  push!(LOAD_PATH,joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"]),".julia",string("v",VERSION.major,".",VERSION.minor))) end')
   julia_void_eval('@static if is_windows()  ENV["HOME"]=joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"])) end')
+
+  jloaddf()
+
+  return(invisible(TRUE))
 }
 
 isJuliaOk <- function() .Call("Julia_is_running", PACKAGE="rjulia")
@@ -45,17 +49,14 @@ r2j <- r_julia <- function(x,y)
   if (is.vector(x) || is.array(x)) {  # Covers list and matrix too
     na = is.na(x)
     if (any(na)) {
-      jloaddf()
       invisible(.Call("R_Julia_NA", x, na, y, PACKAGE="rjulia"))
     } else {
       invisible(.Call("R_Julia", x, y, PACKAGE="rjulia"))
     }
   } else if (is.data.frame(x)) {
-    jloaddf()
     na = lapply(x, is.na)
     invisible(.Call("R_Julia_NA_DataFrame", x, na, y, PACKAGE="rjulia"))
   } else if (is.factor(x)) {
-    jloaddf()
     na = is.na(x)
     invisible(.Call("R_Julia_NA_Factor", x, na, y, PACKAGE="rjulia"))
   } else {
