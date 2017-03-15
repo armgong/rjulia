8c558246603e071192e69334887855dca8ec78e7
phaverty <phaverty@gene.com>
2017-03-15 10:55:00 -0700

    Copied jDo and friends from rjulia2. This way seems to be more robust to errors on the julia side.

--- R/rjulia.R
+++ R/rjulia.R
@@ -1,4 +1,3 @@
-
 ## Initialise Julia
 julia_init <- function(juliahome="", disablegc = FALSE, parallel = TRUE)
 {
@@ -21,28 +20,81 @@ julia_init <- function(juliahome="", disablegc = FALSE, parallel = TRUE)
   return(invisible(TRUE))
 }
 
-isJuliaOk <- function() .Call("Julia_is_running", PACKAGE="rjulia")
+######### From: rjulia2
+
+cstrnull<-function(orgstr)
+{
+  return (c(charToRaw(orgstr),as.raw(0)))
+}
+
+ccall<-function(fname,cmdstr)
+{
+  functionsym<-getNativeSymbolInfo(fname)
+  invisible(.C(functionsym,cstrnull(cmdstr)))
+}
+
 
 .julia_init_if_necessary <- function() {
-  if (!isJuliaOk()) {
+  if (!.isJuliaOk) {
     message("Julia not yet running. Calling julia_init() ...")
     julia_init()
-    if (!isJuliaOk())
+    if (!.isJuliaOk)
       stop("Julia *still* not running. Giving up.")
+    else
+      message("julia_init complete successfully")
   }
 }
+
+jDo<-julia_void_eval<-julia_eval<-function(cmdstr) {
+  .julia_init_if_necessary()
+                                        #first clear julia exception
+  ccall("jl_eval_string",'ccall(:jl_exception_clear,Void,());')
+                                        #second eval julia expression
+  ccall("jl_eval_string",cmdstr)
+                                        #then,try catch and show julia execption
+  ccall("jl_eval_string",
+        'if ccall(:jl_exception_occurred,Ptr{Void},())!=C_NULL
+            rjuliaexception=ccall(:jl_exception_occurred,Any,());
+            showerror(STDERR,rjuliaexception);
+            println("");
+            ccall(:jl_exception_clear,Void,());
+       end')
+}
+
+Init<-julia_init <- function(juliahome="")
+       {
+           ##force change HOME env variable in R, R change HOME to c:\user\username\Documents
+           ##but on window 7+ this should be c:\user\username, and it not change it, julia could not
+           ##find its package and compiled package, so let us change it
+           if (Sys.info()[['sysname']]=="Windows")
+           {
+               Sys.setenv(HOME=paste0(Sys.getenv("HOMEDRIVE"),Sys.getenv("HOMEPATH")))
+           }
+
+           juliabindir <- if (nchar(juliahome) > 0) juliahome else {
+                                                                      gsub("\"", "",
+                                                                      system('julia -E JULIA_HOME',
+                                                                             intern=TRUE))
+                                                              }
+           ccall("jl_init",juliabindir)
+           .isJuliaOk<<-T
+           ## If on Windows, run a specific push to compensate for R not handling pkg.dir() correctly.
+             ##jDo('@windows_only
+           ##push!(LOAD_PATH,joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"]),".julia",string("v",VERSION.major,".",VERSION.minor)))')
+           ##jDo('@windows_only ENV["HOME"]=joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"]))')
+           ## Loading julia packages
+           jDo("using DataFrames")
+           jDo("using RCall")
+       }
+
+
+########## End: from rjulia2
 
 j2r <- julia_eval <- function(expression)
 {
   .julia_init_if_necessary()
   ## Evaluate the expression and return the results of that evaluation.
   .Call("jl_eval", expression, PACKAGE="rjulia")
-}
-
-jDo <- julia_void_eval <- function(expression)
-{
-  .julia_init_if_necessary()
-  invisible(.Call("jl_void_eval",expression, PACKAGE="rjulia"))
 }
 
 r2j <- r_julia <- function(x,y)
