f24d4091bd8cc36043a81bbcabc6b6a75b02eed5
phaverty <phaverty@gene.com>
2017-08-17 16:28:41 -0700

    Fixes #58, picked one name for each function

--- R/rjulia.R
+++ R/rjulia.R
@@ -1,28 +1,35 @@
 #' @name rjulia package
 #' @title Integrating R and Julia -- Calling Julia from R
 #' @description
-#' Provides a mechanism to send data to create a running julia proess,
-#' send and receive data to/from that process and run arbitrary code in
-#' the process.
+#' RJulia (R package name \pkg{rjulia}) provides an interface between \R
+#' and Julia by converting \R and Julia objects mutually,  and running
+#' Julia scripts from \R.
+#'
+#' \R is a sophisticated feature-rich statistical software but a bit
+#' slow,  julia is new advanced computing language which is fast and
+#' LLVM-based JIT. So, combining \R and julia will allow us to do better
+#' statistical computing.
 #' @docType package
 #' @useDynLib rjulia
+#' @family rjulia
 NULL
 
-#' Initialise Julia
+#' Start Julia from R,  properly initializing the  R--Julia Interface
 #'
-#' Initialise Julia
+#' Start Julia from R,  properly initializing the  R--Julia Interface
 #' @param disablegc single logical, turn of garbage collection?
 #' @return TRUE, invisibly
 #' @export julia_init
+#' @family rjulia
 julia_init <- function(disablegc = FALSE) {
   ## Otherwise, initialise Julia using the provided home directory.
   .Call("initJulia", disablegc, PACKAGE = "rjulia")
 
   ## If on Windows, run a specific push to compensate for R not handling pkg.dir() correctly.
-  julia_void_eval('@static if is_windows()  push!(LOAD_PATH,joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"]),".julia",string("v",VERSION.major,".",VERSION.minor))) end')
-  julia_void_eval('@static if is_windows()  ENV["HOME"]=joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"])) end')
+  jDo('@static if is_windows()  push!(LOAD_PATH,joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"]),".julia",string("v",VERSION.major,".",VERSION.minor))) end')
+  jDo('@static if is_windows()  ENV["HOME"]=joinpath(string(ENV["HOMEDRIVE"],ENV["HOMEPATH"])) end')
 
-  if (julia_eval('VERSION < v"0.6.0"'))
+  if (j2r('VERSION < v"0.6.0"'))
       stop("Julia version must be 0.6 or higher.")
 
   return(invisible(TRUE))
@@ -42,8 +49,9 @@ ccall <- function(fname,cmdstr) {
 #' Execute julia code
 #' @param cmdstr single character
 #' @return nothing
-#' @export jDo julia_void_eval
-jDo <- julia_void_eval <- function(cmdstr) {
+#' @export jDo
+#' @family rjulia
+jDo <- function(cmdstr) {
   #first clear julia exception
   ccall("jl_eval_string",'ccall(:jl_exception_clear,Void,());')
   #second eval julia expression
@@ -58,13 +66,21 @@ jDo <- julia_void_eval <- function(cmdstr) {
        end')
 }
 
-#' Evaluate julia code and return result to R
+#' Evaluate Julia Code or Get a Julia Variable
 #'
-#' Evaluate julia code and return result to R
+#'  Evaluate Julia code from \R, or simply get a Julia variable into \R. Whereas
+#' \code{j2r()} is currently \emph{identical} to \code{j2r()},
+#' \code{jDo()} will evaluate code in the running julia
+#' process \emph{without} returning a value.
 #' @param expression single character
 #' @return single character
-#' @export j2r julia_eval
-j2r <- julia_eval <- function(expression) {
+#' @examples
+#' j2r("cos(pi * (1:7))")
+#' j2r("map(x -> x^2,  (7, 17, 47))") # tuple in Julia ==>  list (with no names!) in R
+#' j2r("rand(2, 3)")# or rather use R's RNG
+#' @export j2r
+#' @family rjulia
+j2r <- function(expression) {
   .Call("jl_eval", expression, PACKAGE="rjulia")
 }
 
@@ -75,8 +91,23 @@ j2r <- julia_eval <- function(expression) {
 #' @param x R variable: vector, array, factor, list, data.frame
 #' @param y single character, symbol to use in julia
 #' @return nothing
-#' @export r2j r_julia
-r2j <- r_julia <- function(x,y) {
+#' @examples
+#' r2j(pi,  "Rpi")
+#' j2r("pi") # Julia's builtin pi
+#' ## is there a difference
+#' j2r("Rpi - pi") # typically 0
+#'
+#' ## integer vector:
+#' r2j(1:10,  "i10")
+#' j2r("i10") ; stopifnot(identical(1:10,  j2r("i10")))
+#'
+#' ## matrix,  passed to Julia and got back
+#' m <- matrix(exp(-5:6),  3, 4)
+#' r2j(m,  "m")
+#' stopifnot(identical(m,  j2r("m")))
+#' @export r2j
+#' @family rjulia
+r2j <- function(x,y) {
   if (is.vector(x) || is.array(x)) {  # Covers list and matrix too
 
     if (anyNA(x)) {
@@ -103,8 +134,9 @@ r2j <- r_julia <- function(x,y) {
 #' These packages are loaded any time r2j is used to pass data.frames or
 #' any objects containing NAs.
 #' @return single logical
-#' @export jdfinited julia_DataArrayFrameInited
-jdfinited <- julia_DataArrayFrameInited <- function() {
+#' @export jdfinited
+#' @family rjulia
+jdfinited  <- function() {
   .Call("Julia_DataArrayFrameInited", PACKAGE="rjulia")
 }
 
@@ -113,20 +145,50 @@ jdfinited <- julia_DataArrayFrameInited <- function() {
 #' These packages are loaded any time r2j is used to pass data.frames or
 #' any objects containing NAs, but you can load them early if you want.
 #' @return single logical for success or failure
-#' @export jloaddf julia_LoadDataArrayFrame
-jloaddf <- julia_LoadDataArrayFrame <- function() {
-  invisible(.Call("Julia_LoadDataArrayFrame", PACKAGE="rjulia"))
-  if (!julia_DataArrayFrameInited()) warning(
- "DataArray and DataFrame Julia packages have not been loaded.
-  Please install or check installation directory.")
+#' @export jloaddf
+#' @family rjulia
+jloaddf  <- function() {
+  invisible(.Call("Julia_LoadDataArray", PACKAGE="rjulia"))
+  if (!jdfinited())
+      warning("DataArray and DataFrame Julia packages could not be loaded.")
 }
 
-#' Set option for converting julia BigInt values to doubles
+#' Option for Converting Julia Types Uint32,  Int64,  and Uint64 to R
 #'
-#' Set option for converting julia BigInt values to doubles
-#' @param mode single logical
+#' We need to pay attention on the julia types \code{UInt32}, \code{Int64}
+#'   and \code{UInt64} are mapped to \R. As the \code{\link{integer}} type
+#'   in \R is (only) signed 32 bit, the above three types contain values out
+#'   of \R's integer range.
+#'   \code{julia_BigintToDouble()} allows to determine what happens when
+#'   such variables are converted to \R objects.
+#' @param mode single logical, indicates how to convert Uint32,
+#'   Int64 and Uint64 julia objects to \R. If \code{mode} is true, convert to
+#'   double. If it is false, the julia object will be converted to
+#'   integer or double depending on the object's value. If all values of
+#'   the object stay within the range of signed int32, then the object will be
+#'   converted to integer, otherwise to double. The default \code{mode}
+#'   is \code{FALSE}.
 #' @return NULL
+#' @examples
+#' julia_BigintToDouble(TRUE)
+#' y <- j2r("jvar=convert(Uint64,  12)")
+#' julia_void_eval("println(typeof(jvar))") # UInt64
+#' str(y) # num 12 -- double ("TRUE" above)
+#'
+#' julia_BigintToDouble(FALSE)
+#' y <- j2r("jvar=convert(Uint64,  31)")
+#' julia_void_eval("println(typeof(jvar))") # UInt64
+#' str(y) # int 31 -- integer,  not double  ("FALSE" above)
+#'
+#' ## Show maximal values of the 64-bit integer types:
+#' y[1] <- j2r("jvar= convert(Uint64,  typemax(Uint64))")
+#' y[2] <- j2r("jv2 = convert(Int64,   typemax( Int64))")
+#' julia_void_eval("println(typeof(jvar), ' ',  typeof(jv2))")
+#' str(y) # num 1.84e+19 9.22e+18
+#' ## they are 2^64 and 2^63  respectively:
+#' stopifnot(log2(y) == 64:63)
 #' @export julia_BigintToDouble
+#' @family rjulia
 julia_BigintToDouble <- function(mode = FALSE) {
   invisible(.Call("Julia_BigintToDouble", mode, PACKAGE="rjulia"))
 }
